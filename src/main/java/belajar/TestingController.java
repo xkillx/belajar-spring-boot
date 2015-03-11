06e49e5acccca718a5250aa179ef1543c92fc4e7
Azhar Khalid <azhar.tkjzone@gmail.com>
2015-03-11 16:37:59 +0700

    First Commit

---
+++ src/main/java/belajar/TestingController.java
@@ -0,0 +1,24 @@
+package belajar;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestingController {
+	
+	@RequestMapping("/")
+	public String hello() {
+		return dummy();
+	}
+
+	private String dummy() {
+		return "Hello World From Dummy";
+	}
+	
+	@RequestMapping("/belajar")
+	public String belajar(@RequestParam(defaultValue = "Azhar") String nama) {
+		return "Hello " + nama;
+	}
+	
+}
